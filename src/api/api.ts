0e3a0adf4fdd764464942d7b0fb874446436ab0b
Sandesh thapa <sandesh.thapa@braintip.ai>
2024-08-25 20:45:01 +0545

    changes made

--- src/api/api.ts
+++ src/api/api.ts
@@ -12,48 +12,48 @@ const api = axios.create({
 });
 
 // request interceptors to add the access token to the headers
-// api.interceptors.request.use(
-//   async (config) => {
-//     const token = localStorage.getItem("token");
-//     if (token) {
-//       config.headers["Authorization"] = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+api.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 // // response interceptors to handle token refresh
-// api.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   async (error) => {
-//     const orginalRequest = error.config;
-//     if (error.response.status === 401 && !orginalRequest._retry) {
-//       orginalRequest._retry = true;
-//       try {
-//         const response = await axios.get(BASE_URL + "/refresh-token", {
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const orginalRequest = error.config;
+    if (error.response.status === 401 && !orginalRequest._retry) {
+      orginalRequest._retry = true;
+      try {
+        const response = await axios.get(BASE_URL + "/refresh-token", {
 
-//           withCredentials: true,
-//           headers: {
-//             "Content-Type": "application/json",
-//           }
-//         });
-//         const { accessToken } = response.data;
-//         localStorage.setItem("token", accessToken);
-//         orginalRequest.headers["Authorization"] = `Bearer ${accessToken}`;
-//         return api(orginalRequest);
-//       } catch (error) {
-//         // Handle refresh token failure (e.g., log out the user)
-//         return Promise.reject(error);
-//       }
-//     }
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          }
+        });
+        const { accessToken } = response.data;
+        localStorage.setItem("token", accessToken);
+        orginalRequest.headers["Authorization"] = `Bearer ${accessToken}`;
+        return api(orginalRequest);
+      } catch (error) {
+        // Handle refresh token failure (e.g., log out the user)
+        return Promise.reject(error);
+      }
+    }
 
-//     return Promise.reject(error);
-//   }
-// );
+    return Promise.reject(error);
+  }
+);
 
 export default api;
