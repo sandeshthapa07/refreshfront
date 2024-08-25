54d29f02bc2c6bcee83aff27f4b84d67558a7d00
Sandesh thapa <sandesh.thapa@braintip.ai>
2024-08-25 21:18:51 +0545

    backend setup

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://roomfinder-hrx5.onrender.com";
 
 const api = axios.create({
   baseURL: BASE_URL,
